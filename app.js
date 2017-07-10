e86e6e3ffd62d3ad8b94731c36550c0440a6398e
taddeimania <jtaddei@gmail.com>
2017-07-10 09:56:36 -0400

    made patch work out of patchwork

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Dog = require("./models/dog");
+const bodyParser = require("body-parser");
+mongoose.Promise = require('bluebird');
+
+mongoose.connect("mongodb://localhost:27017/dogdb");
 
 // app.use("static", express.static("public"));
 const dogs = [{
@@ -8,13 +14,34 @@ const dogs = [{
   {name: "scooter", age: 16, id: 2},
   {name: "paul", age: 20, id: 3}
 ];
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/api/dogs", (req, res) => {
-  res.json(dogs);
+  Dog.find({}).then(dogs => {
+    res.json(dogs)
+    console.log(dogs);
+  })
+});
+
+app.post("/api/dogs", (req, res) => {
+  dog = new Dog(req.body).save().then((newDog) => {
+    res.json({});
+  })
+});
+
+app.patch("/api/dogs/:id", (req, res) => {
+  Dog.findOne({_id: req.params.id}).then(dog => {
+    Object.keys(req.body).forEach(key => {
+      dog[key] = req.body[key];
+    });
+    dog.save().then(dog => {
+      res.json(dog);
+    });
+  });
 });
 
 app.get("/api/dogs/:id", (req, res) => {
